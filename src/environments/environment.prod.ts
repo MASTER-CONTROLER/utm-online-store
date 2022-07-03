f390e61c3404c3852c0c62fefdd03fba44306432
Ahmad Almunajjed <AhmadAlMunajjed@outlook.com>
2022-07-03 19:39:06 +0300

    update firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
 	production: false,
 	firebase: {
-		apiKey: "",
-		authDomain: "",
-		databaseURL: "",
-		projectId: "",
-		storageBucket: "",
-		messagingSenderId: "",
-		appId: "",
-		measurementId: ""
+		apiKey: "AIzaSyAHG-1nkRP0Whi7x1nqdDL8c0m4RvW5niM",
+		authDomain: "utm-apps.firebaseapp.com",
+		projectId: "utm-apps",
+		storageBucket: "utm-apps.appspot.com",
+		messagingSenderId: "780649670698",
+		appId: "1:780649670698:web:c5a175aeb2093f73c87b53",
+		measurementId: "G-P7958NZ27Z"
 	}
 };
